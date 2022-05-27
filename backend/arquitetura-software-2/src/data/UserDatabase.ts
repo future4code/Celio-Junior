3347cd6082810f99aa6f0fb76f777ff43cea4e8c
Defaultao <celiojr932ster@gmail.com>
2022-05-26 23:03:38 -0300

    Exercícios arquitetura de software 2

---
+++ backend/arquitetura-software-2/src/data/UserDatabase.ts
@@ -0,0 +1,23 @@
+import { User } from "../types/User";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  private static TABLE_NAME = "LABEFLIX_USER";
+
+  async create({ id, name, email, password }: any): Promise<void> {
+    await UserDatabase.connection
+      .insert({
+        id,
+        name,
+        email,
+        password,
+      })
+      .into(UserDatabase.TABLE_NAME);
+  }
+
+  public getAll = async (): Promise<User[]> => {
+    const getUser = await UserDatabase.connection(UserDatabase.TABLE_NAME)
+    .select("*")
+    return getUser
+  }
+}
